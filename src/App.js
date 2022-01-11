22b8831cfbad4e948817e150d73e87f65effd8a7
Manish Jha <manishjhazyx@gmail.com>
2022-01-12 00:13:34 +0530

    added header and post components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import './App.css';
+import Post from './Post.js';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Friend!</h1>
+    <div className="app">
+     
 
       <div className='app__header'>
-
-        <img className='app__headerImage' src='https://instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png'/>
-
+        <img className='app__headerImage' src='https://pngimg.com/uploads/instagram/instagram_PNG5.png' alt='' />
       </div>
+
+      <Post />
+
+      <Post />
+      <Post />
+      <Post />
+
+      <h1>Hello Friend!</h1>
     </div>
   );
 }
-
+  //  https://instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png
 export default App;
